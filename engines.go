a57cc060508c1784346aecf7cf23175d21bb1090
kasiss <xueyezhishang@163.com>
2018-06-25 17:32:50 +0800

    调整文件处理引擎使用的内容输出方法

--- engines.go
+++ engines.go
@@ -4,153 +4,11 @@
 package smile
 
 import (
-	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
 	"strings"
 )
-
-//常见的文件后缀对应的输出类型
-var CONTENTTYPE = map[string]string{
-	"hqx":     "application/mac-binhex40",
-	"cpt":     "application/mac-compactpro",
-	"doc":     "application/msword",
-	"bin":     "application/octet-stream",
-	"dms":     "application/octet-stream",
-	"lha":     "application/octet-stream",
-	"lzh":     "application/octet-stream",
-	"exe":     "application/octet-stream",
-	"class":   "application/octet-stream",
-	"so":      "application/octet-stream",
-	"dll":     "application/octet-stream",
-	"oda":     "application/oda",
-	"pdf":     "application/pdf",
-	"ai":      "application/postscript",
-	"eps":     "application/postscript",
-	"ps":      "application/postscript",
-	"smi":     "application/smil",
-	"smil":    "application/smil",
-	"mif":     "application/vnd.mif",
-	"xls":     "application/vnd.ms-excel",
-	"ppt":     "application/vnd.ms-powerpoint",
-	"wbxml":   "application/vnd.wap.wbxml",
-	"wmlc":    "application/vnd.wap.wmlc",
-	"wmlsc":   "application/vnd.wap.wmlscriptc",
-	"bcpio":   "application/x-bcpio",
-	"vcd":     "application/x-cdlink",
-	"pgn":     "application/x-chess-pgn",
-	"cpio":    "application/x-cpio",
-	"csh":     "application/x-csh",
-	"dcr":     "application/x-director",
-	"dir":     "application/x-director",
-	"dxr":     "application/x-director",
-	"dvi":     "application/x-dvi",
-	"spl":     "application/x-futuresplash",
-	"gtar":    "application/x-gtar",
-	"hdf":     "application/x-hdf",
-	"js":      "application/x-javascript",
-	"skp":     "application/x-koan",
-	"skd":     "application/x-koan",
-	"skt":     "application/x-koan",
-	"skm":     "application/x-koan",
-	"latex":   "application/x-latex",
-	"nc":      "application/x-netcdf",
-	"cdf":     "application/x-netcdf",
-	"sh":      "application/x-sh",
-	"shar":    "application/x-shar",
-	"swf":     "application/x-shockwave-flash",
-	"sit":     "application/x-stuffit",
-	"sv4cpio": "application/x-sv4cpio",
-	"sv4crc":  "application/x-sv4crc",
-	"tar":     "application/x-tar",
-	"tcl":     "application/x-tcl",
-	"tex":     "application/x-tex",
-	"texinfo": "application/x-texinfo",
-	"texi":    "application/x-texinfo",
-	"t":       "application/x-troff",
-	"tr":      "application/x-troff",
-	"roff":    "application/x-troff",
-	"man":     "application/x-troff-man",
-	"me":      "application/x-troff-me",
-	"ms":      "application/x-troff-ms",
-	"ustar":   "application/x-ustar",
-	"src":     "application/x-wais-source",
-	"xhtml":   "application/xhtml+xml",
-	"xht":     "application/xhtml+xml",
-	"zip":     "application/zip",
-	"au":      "audio/basic",
-	"snd":     "audio/basic",
-	"mid":     "audio/midi",
-	"midi":    "audio/midi",
-	"kar":     "audio/midi",
-	"mpga":    "audio/mpeg",
-	"mp2":     "audio/mpeg",
-	"mp3":     "audio/mpeg",
-	"aif":     "audio/x-aiff",
-	"aiff":    "audio/x-aiff",
-	"aifc":    "audio/x-aiff",
-	"m3u":     "audio/x-mpegurl",
-	"ram":     "audio/x-pn-realaudio",
-	"rm":      "audio/x-pn-realaudio",
-	"rpm":     "audio/x-pn-realaudio-plugin",
-	"ra":      "audio/x-realaudio",
-	"wav":     "audio/x-wav",
-	"pdb":     "chemical/x-pdb",
-	"xyz":     "chemical/x-xyz",
-	"bmp":     "image/bmp",
-	"gif":     "image/gif",
-	"ico":     "image/x-icon",
-	"ief":     "image/ief",
-	"jpeg":    "image/jpeg",
-	"jpg":     "image/jpeg",
-	"jpe":     "image/jpeg",
-	"png":     "image/png",
-	"tiff":    "image/tiff",
-	"tif":     "image/tiff",
-	"djvu":    "image/vnd.djvu",
-	"djv":     "image/vnd.djvu",
-	"wbmp":    "image/vnd.wap.wbmp",
-	"ras":     "image/x-cmu-raster",
-	"pnm":     "image/x-portable-anymap",
-	"pbm":     "image/x-portable-bitmap",
-	"pgm":     "image/x-portable-graymap",
-	"ppm":     "image/x-portable-pixmap",
-	"rgb":     "image/x-rgb",
-	"xbm":     "image/x-xbitmap",
-	"xpm":     "image/x-xpixmap",
-	"xwd":     "image/x-xwindowdump",
-	"igs":     "model/iges",
-	"iges":    "model/iges",
-	"msh":     "model/mesh",
-	"mesh":    "model/mesh",
-	"silo":    "model/mesh",
-	"wrl":     "model/vrml",
-	"vrml":    "model/vrml",
-	"css":     "text/css",
-	"html":    "text/html",
-	"htm":     "text/html",
-	"asc":     "text/plain",
-	"txt":     "text/plain",
-	"rtx":     "text/richtext",
-	"rtf":     "text/rtf",
-	"sgml":    "text/sgml",
-	"sgm":     "text/sgml",
-	"tsv":     "text/tab-separated-values",
-	"wml":     "text/vnd.wap.wml",
-	"wmls":    "text/vnd.wap.wmlscript",
-	"etx":     "text/x-setext",
-	"xsl":     "text/xml",
-	"xml":     "text/xml",
-	"mpeg":    "video/mpeg",
-	"mpg":     "video/mpeg",
-	"mpe":     "video/mpeg",
-	"qt":      "video/quicktime",
-	"mov":     "video/quicktime",
-	"mxu":     "video/vnd.mpegurl",
-	"avi":     "video/x-msvideo",
-	"movie":   "video/x-sgi-movie",
-	"ice":     "x-conference/x-cooltalk",
-}
 
 //注册几种请求类型
 const (
@@ -203,20 +61,8 @@ func (f *FileEngine) Init(c *Combination) IEngine {
 
 //响应处理方法
 func (f *FileEngine) Handle() (err error) {
-	//根据不同的后缀 输出不同的请求头
-	//目前只涉及有常见后缀文件
-	ext := strings.Trim(f.FileExt, ".")
-	if contentType, ok := CONTENTTYPE[ext]; ok {
-		f.cb.SetHeader("Content-Type", contentType)
-	} else {
-		f.cb.SetHeader("Content-Type", "text/plain")
-	}
-	//读取文件内容 发送到客户端
-	f.readFile()
-
-	if err != nil {
-		return err
-	}
+	//调用http包输出文件的方法
+	http.ServeFile(f.cb.ResponseWriter, f.cb.Request, f.FilePath)
 	return nil
 }
 
@@ -237,23 +83,6 @@ func (f *FileEngine) FileExist() bool {
 //判断请求是否为文件请求
 func (f *FileEngine) Check(i interface{}) bool {
 	return f.FileExist()
-}
-
-//读取文件内容
-func (f *FileEngine) readFile() {
-	file, err := os.Open(f.FilePath)
-	if err != nil {
-		//如果文件存在 但打开文件失败 则返回error
-		return
-	}
-	defer file.Close()
-	//读取文件内容并返回
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		return
-	}
-
-	f.cb.Write(content)
 }
 
 //获取引擎结构类型
